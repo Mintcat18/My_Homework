5caddbab45db8941f275db9df6c6bd068471ee06
Anastasia Voevodova <n.voevodova@yandex.ru>
2023-02-11 21:29:13 +0300

    tasks__

---
+++ Task_/Program.cs
@@ -0,0 +1,14 @@
+﻿//Подсчитать сумму цифр в числе
+
+Console.Clear();
+
+Console.WriteLine("Введите число ");
+int A = int.Parse(Console.ReadLine()!);
+int Sum = 0;
+
+while (A > 0)
+{
+    Sum = Sum + A % 10;
+    A = A / 10;
+}
+Console.WriteLine(Sum);
---
+++ Task__/Program.cs
@@ -0,0 +1,14 @@
+﻿//Написать программу вычисления произведения чисел от 1 до N
+
+Console.Write("Введите число ");
+int num = int.Parse(Console.ReadLine());
+int count = 1;
+int a = 1;
+
+while (count <= num)
+{
+    count = count * a;
+    Console.Write($"{a} ");
+    a++;
+}
+Console.WriteLine($"{count}");
