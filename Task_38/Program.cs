88cce618f6617fbd59f14d6400223332b1121eb8
Anastasia Voevodova <n.voevodova@yandex.ru>
2023-02-17 20:35:59 +0300

    tasks

--- Task_38/Program.cs
+++ Task_38/Program.cs
@@ -1,5 +1,5 @@
 ﻿/*Задайте массив вещественных чисел. 
-Найдите разницу между максимальным и минимальным элементов массива.
+Найдите разницу между максимальным и минимальным элементами массива.
 [3 7 22 2 78] -> 76*/
 Console.Clear();
 double[] GetArray(int size, double minValue, double maxValue)
@@ -7,33 +7,37 @@ double[] GetArray(int size, double minValue, double maxValue)
     double[] result = new double[size];
     for (int i = 0; i < size; i++)
     {
-        result[i] = new Random().NextDouble() * (maxValue - minValue) + minValue;
+        result[i] = Math.Round(new Random().NextDouble() * (maxValue - minValue) + minValue, 2);
     }
     return result;
 }
-double Result(double[] inArray)
+double ResultMax(double[] inArray);
 {
-    double min = inArray[0];
     double max = inArray[0];
     for (int i = 0; i < inArray.Length; i++)
     {
         if (inArray[i] > max)
-        {
-            max = inArray[i];
-        }
-        else if (inArray[i] < min)
-        {
-            min = inArray[i];
-        }
+
+        max = inArray[0];
     }
     return max;
+}
+double ResultMin(double[] inArray)
+{
+    double min = inArray[0];
+    for (int i = 0; i < inArray.Length; i++)
+    {
+        if (inArray[i] < min)
+
+            min = inArray[i];
+    }
     return min;
 }
 
 
 double[] array = GetArray(10, -10, 10);
 Console.WriteLine(String.Join(", ", array));
-double min = Result(array);
-double max = Result(array);
+double min = ResultMax(array);
+double max = ResultMin(array);
 Console.WriteLine($"Максимальное = {max}. Минимальное = {min}");
-
+Console.WriteLine($"Разница между максимальным и минимальным = {Math.Round(max - min, 2)}");
