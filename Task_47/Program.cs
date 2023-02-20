c34f7b192d8ca4926f39bee872cd81229d4ff3e4
Anastasia Voevodova <n.voevodova@yandex.ru>
2023-02-20 20:37:47 +0300

    new tasks 20.02.23

---
+++ Task_47/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+Console.Clear();
+int[,] GetArray(int m, int n, double minValue, double maxValue)
+{
+    double[,] result = new double[m, n];
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = Math.Round(new Random().NextDouble() * (maxValue - minValue) + minValue, 2);
+        }
+    }
+    return result;
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
+
+Console.WriteLine("введите количество строк");
+int pos = int.Parse(Console.ReadLine());
+Console.WriteLine("введите количество столбцов");
+int col = int.Parse(Console.ReadLine());
+double[,] numbers = new double[pos, col];
+
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
---
+++ Task_52/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+Console.Clear();
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return result;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
+
+void ElemSum(int[,] inArray)
+{
+    for (int j = 0; j < inArray.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < inArray.GetLength(0); i++)
+        {
+            sum += inArray[i, j];
+        }
+        double arithmSum = Math.Round(sum / inArray.GetLength(0), 2);
+        Console.Write($"{arithmSum},");
+    }
+}
+
+Console.WriteLine("введите количество строк ");
+int pos = int.Parse(Console.ReadLine()!);
+Console.WriteLine("введите номер столбца");
+int col = int.Parse(Console.ReadLine()!);
+int[,] array = GetArray(pos, col, -5, 25);
+PrintArray(array);
+ElemSum(array);
+
+
+
